7f06f6620f0caf4870179ca99baca922208c53f2
kroissharp <mikx0067@edu.easj.dk>
2018-02-08 14:07:14 +0100

    mindre ændring lavet = om til ? for resultat view

--- app/src/main/java/mik/easj/skole/exercisecalculator/MainActivity.java
+++ app/src/main/java/mik/easj/skole/exercisecalculator/MainActivity.java
@@ -5,27 +5,70 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 private int sum;
+private static String tegn;
+
+private int regnemetode(){
+    //inputs
+    EditText input1 = (EditText) findViewById(R.id.Input1);
+    EditText input2 = (EditText) findViewById(R.id.Input2);
+
+
+    //udføre koden
+
+    if (tegn == "+"){
+        sum = Integer.parseInt(input1.getText().toString())+Integer.parseInt(input2.getText().toString());
+    }
+    if (tegn == "*"){
+        sum = Integer.parseInt(input1.getText().toString())*Integer.parseInt(input2.getText().toString());
+        }
+    if (tegn == "-"){
+        sum = Integer.parseInt(input1.getText().toString())-Integer.parseInt(input2.getText().toString());
+    }
+    if (tegn == "/"){
+        sum = Integer.parseInt(input1.getText().toString())/Integer.parseInt(input2.getText().toString());
+    }
+    else
+        Toast.makeText(this, "vælg hvad du ønsker at regne med", Toast.LENGTH_SHORT).show();
+
+    return sum;
+
+}
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void Beregn(View view) {
-        //inputs
-        EditText input1 = (EditText) findViewById(R.id.Input1);
-        EditText input2 = (EditText) findViewById(R.id.Input2);
+    public void plusbtn(View view) {
+       tegn = "+";
+    }
+
+    public void minusbtn(View view) {
+        tegn = "-";
+    }
+
+    public void gangebtn(View view) {
+        tegn = "*";
+    }
+
+    public void dividerebtn(View view) {
+        tegn = "/";
+    }
+
+    public void LigmedBtn(View view) {
         //vores set text
         TextView Resultat = (TextView) findViewById(R.id.Resultat);
 
-        //udføre koden
+    int sum = regnemetode();
+        Resultat.setText(Integer.toString(sum));
 
-       sum = Integer.parseInt(input1.getText().toString())+Integer.parseInt(input2.getText().toString());
 
-       Resultat.setText(Integer.toString(sum));
     }
 }
